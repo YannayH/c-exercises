ccef8cd55fc2df55df4fe1e0d5f9c63480fdfdf7
Yannay <yannayha@gmail.com>
2020-03-16 19:52:52 +0200

    Some modifications to exercise 5.

--- memmanager.c
+++ memmanager.c
@@ -1,53 +1,40 @@
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
-#include <stdio.h>
 
 #include "memmanager.h"
 
-#define MAXIMUM_ALLOCATIONS 10
+#define MAXIMUM_ALLOCATIONS 1024 * 1024
 
-void* LIBRARY_NOT_INITIALIZED = (void*) 0;
-void* NOT_ENOUGH_MEMORY = (void*) -1;
-void* FRAGMENTED_MEMORY = (void*) -2;
+static size_t BLOCK_SIZE = 2147483647;
+static void* main_block = NULL;
+static struct AllocatedMemory **allocated_memory_blocks = NULL;
 
-
-void* main_block = NULL;
-size_t main_block_size = 0;
-struct AllocatedMemory **allocated_memory_blocks = NULL;
-
-void initialize_manager(size_t block_size) {
-    main_block = malloc(block_size);
-    main_block_size = block_size;
+__attribute__((constructor))
+static void initialize_manager() {
+    main_block = malloc(BLOCK_SIZE);
     allocated_memory_blocks = malloc(sizeof(struct AllocatedMemory*) * MAXIMUM_ALLOCATIONS);
 
-    memset(main_block, 0, block_size);
+    memset(main_block, 0, BLOCK_SIZE);
     for (size_t i = 0; i < MAXIMUM_ALLOCATIONS; i ++) {
         allocated_memory_blocks[i] = NULL;
     }
 }
 
 void* allocate_memory(size_t size) {
-    if (main_block == NULL) {
-        return LIBRARY_NOT_INITIALIZED;
-    }
-
-    size_t total_allocation = 0;
     size_t start_address = 0;
 
     for (size_t j = 0; j < MAXIMUM_ALLOCATIONS; j ++) {
         if (allocated_memory_blocks[j] != 0) {
             if (start_address + size >= allocated_memory_blocks[j]->address) {
                 start_address = allocated_memory_blocks[j]->address + allocated_memory_blocks[j]->size;
-                total_allocation += allocated_memory_blocks[j]->size;
             }
         }
     }
 
-    if (main_block_size - start_address < size) {
-        if (main_block_size - total_allocation < size) {
-            return NOT_ENOUGH_MEMORY;
-        }
-        return FRAGMENTED_MEMORY;
+    if (BLOCK_SIZE - start_address < size) {
+        errno = ENOMEM;
+        return NULL;
     }
 
     struct AllocatedMemory* allocated_memory_block = malloc(sizeof(struct AllocatedMemory));
@@ -62,35 +49,6 @@ void* allocate_memory(size_t size) {
     }
 
     return (void*) main_block + allocated_memory_block->address;
-}
-
-int allocated_memory_comp(const void* first, const void* second) {
-    struct AllocatedMemory* first_allocated = (struct AllocatedMemory*) first;
-    struct AllocatedMemory* second_allocated = (struct AllocatedMemory*) second;
-
-    if (*(int*) first != 0 && *(int*)second == 0) {
-        return -1;
-    } else if (*(int*)first == 0 && *(int*)second != 0) {
-        return 1;
-    } else if (*(int*)first == 0 && *(int*)second == 0) {
-        return 0;
-    } else {
-        return (int) (first_allocated->address - second_allocated->address);
-    }
-}
-
-void defragment_memory() {
-    size_t free_address = 0;
-    qsort(allocated_memory_blocks, MAXIMUM_ALLOCATIONS, sizeof (struct AllocatedMemory*), allocated_memory_comp);
-    for (size_t j = 0; j < MAXIMUM_ALLOCATIONS; j ++) {
-        if (allocated_memory_blocks[j] != 0) {
-            if (free_address != allocated_memory_blocks[j]->address) {
-                memcpy(main_block + free_address, main_block + allocated_memory_blocks[j]->address, allocated_memory_blocks[j]->size);
-                allocated_memory_blocks[j]->address = free_address;
-            }
-            free_address += allocated_memory_blocks[j]->size;
-        }
-    }
 }
 
 int free_memory(void* address) {
@@ -110,7 +68,9 @@ int free_memory(void* address) {
     return 1;
 }
 
-void finalize_manager() {
+
+__attribute__((destructor))
+static void finalize_manager() {
     for (size_t j = 0; j < MAXIMUM_ALLOCATIONS; j ++) {
         if (allocated_memory_blocks[j] != 0) {
             free(allocated_memory_blocks[j]);
@@ -123,5 +83,4 @@ void finalize_manager() {
 
     allocated_memory_blocks = NULL;
     main_block = NULL;
-    main_block_size = 0;
 }
--- memmanager.h
+++ memmanager.h
@@ -8,15 +8,8 @@ struct AllocatedMemory {
     uint64_t size;
 };
 
-extern void* LIBRARY_NOT_INITIALIZED;
-extern void* NOT_ENOUGH_MEMORY;
-extern void* FRAGMENTED_MEMORY;
-
-void initialize_manager(size_t block_size);
 void* allocate_memory(size_t size);
-void defragment_memory();
 int free_memory(void* address);
-void finalize_manager();
 
 
 #endif //C_EXERCISE_MEMMANAGER_H
